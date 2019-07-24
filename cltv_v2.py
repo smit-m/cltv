9679819d0f3d738534eb03c294712ba5d164f5e6
Smit Mehta <mehta.smit92@gmail.com>
2019-07-24 13:05:36 -0400

    Create cltv_v2.py

---
+++ cltv_v2.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  6 21:29:08 2019
+
+@author: mehta
+"""
+
+# importing the required modules
+import pandas as pd # for dataframes
+import matplotlib.pyplot as plt # for plotting graphs
+import seaborn as sns # for plotting graphs
+import datetime as dt
+import numpy as np
+
+
+data = pd.read_excel("Online Retail.xlsx")
+data.head()
+
+filtered_data=data[['Country','CustomerID']].drop_duplicates()
+
+#Top ten country's customer
+filtered_data.Country.value_counts()[:10].plot(kind='bar')
+
+uk_data=data[data.Country=='United Kingdom']
+uk_data.info()
+
+uk_data.describe()
+
+uk_data = uk_data[(uk_data['Quantity']>0)]
+uk_data.info()
+
+uk_data=uk_data[['CustomerID','InvoiceDate','InvoiceNo','Quantity','UnitPrice']]
+
+#Calulate total purchase
+uk_data['TotalPurchase'] = uk_data['Quantity'] * uk_data['UnitPrice']
+
+
+uk_data_group=uk_data.groupby('CustomerID').agg({'InvoiceDate': lambda date: (date.max() - date.min()).days,
+                                        'InvoiceNo': lambda num: len(num),
+                                        'Quantity': lambda quant: quant.sum(),
+                                        'TotalPurchase': lambda price: price.sum()})
+
+uk_data_group.head()
+
+uk_data_group.columns=['num_days','num_transactions','num_units','spent_money']
+uk_data_group.head()
+
+
+####################################################################
+
+# Average Order Value
+uk_data_group['avg_order_value']=uk_data_group['spent_money']/uk_data_group['num_transactions']
+
+uk_data_group.head()
+
+
+
+purchase_frequency=sum(uk_data_group['num_transactions'])/uk_data_group.shape[0]
+# Repeat Rate
+repeat_rate=uk_data_group[uk_data_group.num_transactions > 1].shape[0]/uk_data_group.shape[0]
+#Churn Rate
+churn_rate=1-repeat_rate
+purchase_frequency,repeat_rate,churn_rate
+
+# Profit Margin
+uk_data_group['profit_margin']=uk_data_group['spent_money']*0.05
+uk_data_group.head()
+
+####################################################################
+
+# Customer Value
+uk_data_group['CLV']=(uk_data_group['avg_order_value']*purchase_frequency)/churn_rate
+#Customer Lifetime Value
+uk_data_group['cust_lifetime_value']=uk_data_group['CLV']*uk_data_group['profit_margin']
+uk_data_group.head()
+
+uk_data['month_yr'] = uk_data['InvoiceDate'].apply(lambda x: x.strftime('%b-%Y'))
+uk_data.head()
+
+sale=uk_data.pivot_table(index=['CustomerID'],columns=['month_yr'],values='TotalPurchase',aggfunc='sum',fill_value=0).reset_index()
+sale.head()
+
+sale['CLV']=sale.iloc[:,2:].sum(axis=1)
+sale.head()
+
+X=sale[['Dec-2011','Nov-2011', 'Oct-2011','Sep-2011','Aug-2011','Jul-2011']]
+y=sale[['CLV']]
+
+#########################################################
+
+#split training set and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y,random_state=0)
+
+
+# import model
+from sklearn.linear_model import LinearRegression
+
+# instantiate
+linreg = LinearRegression()
+
+# fit the model to the training data (learn the coefficients)
+linreg.fit(X_train, y_train)
+
+# make predictions on the testing set
+y_pred = linreg.predict(X_test)
+# print the intercept and coefficients
+print(linreg.intercept_)
+print(linreg.coef_)
+
+#################################################
+
+from sklearn import metrics
+
+# compute the R Square for model
+print("R-Square:",metrics.r2_score(y_test, y_pred))
+
+# calculate MAE using scikit-learn
+print("MAE:",metrics.mean_absolute_error(y_test,y_pred))
+
+#calculate mean squared error
+print("MSE",metrics.mean_squared_error(y_test, y_pred))
+# compute the RMSE of our predictions
+print("RMSE:",np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+
+
